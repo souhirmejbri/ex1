e66f8a7d6352e9664af375030abeee225bf899c8
souhirmejbri <79633973+souhirmejbri@users.noreply.github.com>
2023-12-07 21:12:21 +0100

    Create ex1.java

---
+++ ex1.java
@@ -0,0 +1,9 @@
+
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class ex1 {
+  public static void main(String[] args) {
+    System.out.println("ex1");
+  }
+}
